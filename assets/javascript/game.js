72ef3fadb95da12adca70e0b86538e3a01f0a36e
michael brown <michaelbrown@michaels-MacBook-Pro.local>
2017-11-18 10:24:18 -0700

    homework files

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+$(document).ready()
+
+var = computerRand
+var = userInput
+var = buttonPress
+var = buttonNumber
+
+var userInput = Math.floor((Math.random() * 12) + 1 ){
+
+$("button1").click(function(){
+	$("buttonNumber").html(userNumber);
+});
+
+$("button2").click(function(){
+	$("buttonNumber").html(userNumber);
+});
+
+$("button3").click(function(){
+	$("buttonNumber").html(userNumber);
+});
+
+$("button4").click(function(){
+	$("buttonNumber").html(userNumber);
+});
+
+};
+
+var computerRand = Math.floor((Math.random() * 120 ) + 19 ){
+	$("#computerRand").html(computerNumber);
+};
+
+
